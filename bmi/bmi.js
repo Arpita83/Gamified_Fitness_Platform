a32026c849d155a0cdaaba8e96ba42b2dfa3452d
Arpita <raiarpita0803@gmail.com>
2023-06-19 20:19:55 +0530

    changed the color or bmi result

--- bmi/bmi.js
+++ bmi/bmi.js
@@ -1,3 +1,20 @@
+// document.getElementById("submit").addEventListener("click", bmi);
+
+// function bmi() {
+//     var cm = parseInt(document.getElementById("cm").value);
+//     var kg = parseFloat(document.getElementById("kg").value);
+//     var bm;
+//     var newCm = parseFloat(cm / 100);
+
+//     bm = kg / (newCm * newCm);
+//     bm = bm.toFixed(1);
+//     console.log(bm);
+
+//     document.getElementById("result").innerHTML = bm; 
+// }
+
+
+
 document.getElementById("submit").addEventListener("click", bmi);
 
 function bmi() {
@@ -10,5 +27,16 @@ function bmi() {
     bm = bm.toFixed(1);
     console.log(bm);
 
-    document.getElementById("result").innerHTML = bm; 
+    var resultElement = document.getElementById("result");
+    resultElement.innerHTML = bm;
+
+    if (bm <= 24.9) {
+        resultElement.style.color = "blue";
+    } else if (bm > 24.9 && bm <= 29.9) {
+        resultElement.style.color = "green";
+    }else if (bm > 24.9 && bm <= 39.9) {
+        resultElement.style.color = "yellow";
+    }else{
+        resultElement.style.color = "red";
+    }
 }
