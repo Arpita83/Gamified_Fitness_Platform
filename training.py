d0bf9cc4c4c3bfad2106c0101e79328bb875a3be
Harxnee <91590046+Harxnee@users.noreply.github.com>
2023-06-23 20:07:32 +0530

    Add files via upload

--- training.py
+++ training.py
@@ -1,98 +1,169 @@
-import nltk
-from nltk.stem import WordNetLemmatizer
-lemmatizer = WordNetLemmatizer()
-import json
+import csv
+import pandas as pd
+from tensorflow import keras
+from sklearn.model_selection import train_test_split
+from data import BodyPart 
+import tensorflow as tf
+import tensorflowjs as tfjs
+
+tfjs_model_dir = 'model'
+
+
+# loading final csv file
+def load_csv(csv_path):
+    df = pd.read_csv(csv_path)
+    df.drop(['filename'],axis=1, inplace=True)
+    classes = df.pop('class_name').unique()
+    y = df.pop('class_no')
+    
+    X = df.astype('float64')
+    y = keras.utils.to_categorical(y)
+    
+    return X, y, classes
+
+
+def get_center_point(landmarks, left_bodypart, right_bodypart):
+    """Calculates the center point of the two given landmarks."""
+    left = tf.gather(landmarks, left_bodypart.value, axis=1)
+    right = tf.gather(landmarks, right_bodypart.value, axis=1)
+    center = left * 0.5 + right * 0.5
+    return center
+
+
+def get_pose_size(landmarks, torso_size_multiplier=2.5):
+    """Calculates pose size.
+
+    It is the maximum of two values:
+    * Torso size multiplied by `torso_size_multiplier`
+    * Maximum distance from pose center to any pose landmark
+    """
+    # Hips center
+    hips_center = get_center_point(landmarks, BodyPart.LEFT_HIP, 
+                                 BodyPart.RIGHT_HIP)
+
+    # Shoulders center
+    shoulders_center = get_center_point(landmarks, BodyPart.LEFT_SHOULDER,
+                                      BodyPart.RIGHT_SHOULDER)
+
+    # Torso size as the minimum body size
+    torso_size = tf.linalg.norm(shoulders_center - hips_center)
+    # Pose center
+    pose_center_new = get_center_point(landmarks, BodyPart.LEFT_HIP, 
+                                     BodyPart.RIGHT_HIP)
+    pose_center_new = tf.expand_dims(pose_center_new, axis=1)
+    # Broadcast the pose center to the same size as the landmark vector to
+    # perform substraction
+    pose_center_new = tf.broadcast_to(pose_center_new,
+                                    [tf.size(landmarks) // (17*2), 17, 2])
+
+    # Dist to pose center
+    d = tf.gather(landmarks - pose_center_new, 0, axis=0,
+                name="dist_to_pose_center")
+    # Max dist to pose center
+    max_dist = tf.reduce_max(tf.linalg.norm(d, axis=0))
+
+    # Normalize scale
+    pose_size = tf.maximum(torso_size * torso_size_multiplier, max_dist)
+    return pose_size
+
+
+
+def normalize_pose_landmarks(landmarks):
+    """Normalizes the landmarks translation by moving the pose center to (0,0) and
+    scaling it to a constant pose size.
+  """
+  # Move landmarks so that the pose center becomes (0,0)
+    pose_center = get_center_point(landmarks, BodyPart.LEFT_HIP, 
+                                 BodyPart.RIGHT_HIP)
+
+    pose_center = tf.expand_dims(pose_center, axis=1)
+    # Broadcast the pose center to the same size as the landmark vector to perform
+    # substraction
+    pose_center = tf.broadcast_to(pose_center, 
+                                [tf.size(landmarks) // (17*2), 17, 2])
+    landmarks = landmarks - pose_center
+
+    # Scale the landmarks to a constant pose size
+    pose_size = get_pose_size(landmarks)
+    landmarks /= pose_size
+    return landmarks
+
+
+def landmarks_to_embedding(landmarks_and_scores):
+    """Converts the input landmarks into a pose embedding."""
+    # Reshape the flat input into a matrix with shape=(17, 3)
+    reshaped_inputs = keras.layers.Reshape((17, 3))(landmarks_and_scores)
+
+    # Normalize landmarks 2D
+    landmarks = normalize_pose_landmarks(reshaped_inputs[:, :, :2])
+    # Flatten the normalized landmark coordinates into a vector
+    embedding = keras.layers.Flatten()(landmarks)
+    return embedding
+
+
+def preprocess_data(X_train):
+    processed_X_train = []
+    for i in range(X_train.shape[0]):
+        embedding = landmarks_to_embedding(tf.reshape(tf.convert_to_tensor(X_train.iloc[i]), (1, 51)))
+        processed_X_train.append(tf.reshape(embedding, (34)))
+    return tf.convert_to_tensor(processed_X_train)
+
+
+X, y, class_names = load_csv('train_data.csv')
+X_train, X_val, y_train, y_val = train_test_split(X, y, test_size=0.15)
+X_test, y_test, _ = load_csv('test_data.csv')
+
+
+processed_X_train = preprocess_data(X_train)
+processed_X_val =  preprocess_data(X_val)
+processed_X_test = preprocess_data(X_test)
+
+inputs = tf.keras.Input(shape=(34))
+layer = keras.layers.Dense(128, activation=tf.nn.relu6)(inputs)
+layer = keras.layers.Dropout(0.5)(layer)
+layer = keras.layers.Dense(64, activation=tf.nn.relu6)(layer)
+layer = keras.layers.Dropout(0.5)(layer)
+outputs = keras.layers.Dense(len(class_names), activation="softmax")(layer)
+
+model = keras.Model(inputs, outputs)
+
+
+model.compile(
+    optimizer='adam',
+    loss='categorical_crossentropy',
+    metrics=['accuracy']
+)
+
+# Add a checkpoint callback to store the checkpoint that has the highest
+# validation accuracy.
+checkpoint_path = "weights.best.hdf5"
+checkpoint = keras.callbacks.ModelCheckpoint(checkpoint_path,
+                             monitor='val_accuracy',
+                             verbose=1,
+                             save_best_only=True,
+                             mode='max')
+earlystopping = keras.callbacks.EarlyStopping(monitor='val_accuracy', 
+                                              patience=20)
+
+# Start training
+print('--------------TRAINING----------------')
+history = model.fit(processed_X_train, y_train,
+                    epochs=200,
+                    batch_size=16,
+                    validation_data=(processed_X_val, y_val),
+                    callbacks=[checkpoint, earlystopping])
+
+
+print('-----------------EVAUATION----------------')
+loss, accuracy = model.evaluate(processed_X_test, y_test)
+print('LOSS: ', loss)
+print("ACCURACY: ", accuracy)
+
+
+#tfjs.converters.save_keras_model(model, tfjs_model_dir)
 import pickle
 
-import numpy as np
-from keras.models import Sequential
-from keras.layers import Dense, Activation, Dropout
-from keras.optimizers import SGD
-import random
-
-words=[]
-classes = []
-documents = []
-ignore_words = ['?', '!']
-data_file = open('data.json').read()
-intents = json.loads(data_file)
-
-
-for intent in intents['intents']:
-    for pattern in intent['patterns']:
-
-        #tokenize each word
-        w = nltk.word_tokenize(pattern)
-        words.extend(w)
-        #add documents in the corpus
-        documents.append((w, intent['tag']))
-
-        # add to our classes list
-        if intent['tag'] not in classes:
-            classes.append(intent['tag'])
-
-# lemmaztize and lower each word and remove duplicates
-words = [lemmatizer.lemmatize(w.lower()) for w in words if w not in ignore_words]
-words = sorted(list(set(words)))
-# sort classes
-classes = sorted(list(set(classes)))
-# documents = combination between patterns and intents
-print (len(documents), "documents")
-# classes = intents
-print (len(classes), "classes", classes)
-# words = all words, vocabulary
-print (len(words), "unique lemmatized words", words)
-
-
-pickle.dump(words,open('texts.pkl','wb'))
-pickle.dump(classes,open('labels.pkl','wb'))
-
-# create our training data
-training = []
-# create an empty array for our output
-output_empty = [0] * len(classes)
-# training set, bag of words for each sentence
-for doc in documents:
-    # initialize our bag of words
-    bag = []
-    # list of tokenized words for the pattern
-    pattern_words = doc[0]
-    # lemmatize each word - create base word, in attempt to represent related words
-    pattern_words = [lemmatizer.lemmatize(word.lower()) for word in pattern_words]
-    # create our bag of words array with 1, if word match found in current pattern
-    for w in words:
-        bag.append(1) if w in pattern_words else bag.append(0)
-    
-    # output is a '0' for each tag and '1' for current tag (for each pattern)
-    output_row = list(output_empty)
-    output_row[classes.index(doc[1])] = 1
-    
-    training.append([bag, output_row])
-# shuffle our features and turn into np.array
-random.shuffle(training)
-training = np.array(training)
-# create train and test lists. X - patterns, Y - intents
-train_x = list(training[:,0])
-train_y = list(training[:,1])
-print("Training data created")
-
-
-# Create model - 3 layers. First layer 128 neurons, second layer 64 neurons and 3rd output layer contains number of neurons
-# equal to number of intents to predict output intent with softmax
-model = Sequential()
-model.add(Dense(128, input_shape=(len(train_x[0]),), activation='relu'))
-model.add(Dropout(0.5))
-model.add(Dense(64, activation='relu'))
-model.add(Dropout(0.5))
-model.add(Dense(len(train_y[0]), activation='softmax'))
-
-# Compile model. Stochastic gradient descent with Nesterov accelerated gradient gives good results for this model
-sgd = SGD(lr=0.01, decay=1e-6, momentum=0.9, nesterov=True)
-model.compile(loss='categorical_crossentropy', optimizer=sgd, metrics=['accuracy'])
-
-#fitting and saving the model 
-hist = model.fit(np.array(train_x), np.array(train_y), epochs=200, batch_size=5, verbose=1)
-model.save('model.h5', hist)
-
-print("model created")
-
+# Assuming you have a Keras model named 'model' and a file path 'model.pkl'
+with open('model.pkl', 'wb') as f:
+    pickle.dump(model, f)
+print('tfjs model saved at ',tfjs_model_dir)
